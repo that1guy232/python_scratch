52a97ac3d3abfeb9960a39ccd74b3dbec5f3b3be
Tree <keithmaccuish5@gmail.com>
2024-01-07 15:15:51 -0700

    updates

--- new_project.py
+++ new_project.py
@@ -2,11 +2,13 @@ import os
 import shutil
 import sys
 
+
 def copy_and_rename(src_dir, dest_name):
     dest_dir = os.path.join(os.path.dirname(src_dir), dest_name)
     shutil.copytree(src_dir, dest_dir)
     print(f'Copied {src_dir} to {dest_name}')
     return dest_dir
+
 
 def create_settings_file(dir_path, project_name):
     settings_content = f"""
@@ -16,18 +18,34 @@ SCREEN_WIDTH = 16 * SCREEN_SCALE  # 16 * 64 = 1024
 SCREEN_HEIGHT = 9 * SCREEN_SCALE  # 9 * 64 = 576
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 FPS = 90
+
 """
     with open(os.path.join(dir_path, 'settings.py'), 'w') as settings_file:
         settings_file.write(settings_content)
 
-if __name__ == "__main__":
+
+def create_project(project_name):
+    print(f"Creating new project {project_name}")
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    empty_project_path = os.path.join(current_dir, 'empty_project')
+    if not os.path.exists(empty_project_path):
+        print(f"Source directory {empty_project_path} does not exist.")
+        sys.exit(1)
+
+    new_dir = copy_and_rename(empty_project_path, project_name)
+    create_settings_file(new_dir, project_name)
+
+
+    pass
+
+def main():
     if len(sys.argv) != 2:
         print("Usage: python new_project.py [new_project_name]")
         sys.exit(1)
 
     new_project_name = sys.argv[1]
     current_dir = os.path.dirname(os.path.abspath(__file__))
-   
+
     empty_project_path = os.path.join(current_dir, 'empty_project')
 
     if not os.path.exists(empty_project_path):
@@ -36,3 +54,7 @@ if __name__ == "__main__":
 
     new_dir = copy_and_rename(empty_project_path, new_project_name)
     create_settings_file(new_dir, new_project_name)
+
+
+if __name__ == "__main__":
+    main()
--- projects.py
+++ projects.py
@@ -4,50 +4,103 @@ import pygame_gui
 import os
 import subprocess
 
-
 from projects_settings import *
+from new_project import create_project
 
 
 class App:
     def __init__(self):
+        self.folders = None
         pygame.init()
 
         self.delta_time = 0
 
+        # scaled
         self.screen = pygame.display.set_mode(SCREEN_SIZE)
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Projects")
         self.running = True
 
         self.cur_dir = os.path.dirname(__file__)
+
+        self.ui_manager = pygame_gui.UIManager(SCREEN_SIZE)
+
+        self.projects_scrolling_container = pygame_gui.elements.UIScrollingContainer(
+            pygame.Rect(0, 0, 265, SCREEN_SIZE[1] - 50),
+            manager=self.ui_manager,
+        )
+
+        self.new_project_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(
+                270,
+                6,
+                150,
+                50,
+            ),
+            text="New Project",
+            manager=self.ui_manager,
+        )
+        self.new_project_name = pygame_gui.elements.UITextEntryLine(
+            relative_rect=pygame.Rect(
+                270 + 150 + 5,
+                6,
+                150,
+                50,
+            ),
+            manager=self.ui_manager,
+        )
+
+        # create a button for each folder
+        self.project_buttons = []
+        self.kill_buttons = []
+
+        self.update_folders()
+
+        self.processes = {}  # dictionary to store Popen objects
+
+    def update_folders(self):
         self.folders = [
             f
             for f in os.listdir(self.cur_dir)
             if os.path.isdir(os.path.join(self.cur_dir, f))
         ]
 
-        self.remove_folders = ["__pycache__", ".git", "empty_project"]
-        self.folders = [f for f in self.folders if f not in self.remove_folders]
+        remove_folders = ["__pycache__", ".git", "empty_project", ".github", ".idea"]
+        self.folders = [f for f in self.folders if f not in remove_folders]
 
-        print(self.folders)
-
-        self.ui_manager = pygame_gui.UIManager(SCREEN_SIZE)
-
-        # create a button for each folder
-        self.buttons = []
         for i, folder in enumerate(self.folders):
-            self.buttons.append(
+            self.project_buttons.append(
                 pygame_gui.elements.UIButton(
                     relative_rect=pygame.Rect(
-                        0,
+                        5,
                         i * 50,
-                        100,
+                        150,
                         50,
                     ),
                     text=folder,
                     manager=self.ui_manager,
+                    container=self.projects_scrolling_container,  # pass panel as container
                 )
             )
+
+            kill_button = pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect(
+                    155,
+                    i * 50,
+                    100,
+                    50,
+                ),
+                text="Kill",
+                manager=self.ui_manager,
+                container=self.projects_scrolling_container,  # pass panel as container
+            )
+
+            kill_button.disable()  # disable "Kill" button by default
+            self.kill_buttons.append(kill_button)
+
+        self.projects_scrolling_container.set_scrollable_area_dimensions(
+            (245, len(self.folders) * 50)
+        )
 
     def run(self):
         while self.running:
@@ -62,16 +115,34 @@ class App:
                 self.running = False
 
             # handle button events
-            if event.type == pygame.USEREVENT:
-                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                    for i, button in enumerate(self.buttons):
-                        if event.ui_element == button:
-                            print(self.folders[i])
-                            # can we start a new process here?
-                            subprocess.Popen(
-                                ["python", os.path.join(self.folders[i], "main.py")]
-                            )
-                            # os.system(f"python {self.folders[i]}/main.py")
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                for i, button in enumerate(self.project_buttons):
+                    if event.ui_element == button:
+                        p = subprocess.Popen(
+                            ["python", os.path.join(self.folders[i], "main.py")]
+                        )
+                        self.processes[button] = p  # store the Popen object
+                        self.kill_buttons[
+                            i
+                        ].enable()  # enable corresponding "Kill" button
+                        button.disable()  # disable main button after it's pressed
+
+                # check if a "kill" button was pressed
+                for i, button in enumerate(self.kill_buttons):
+                    if event.ui_element == button:
+                        if self.project_buttons[i] in self.processes:
+                            self.processes[self.project_buttons[i]].terminate()
+                            del self.processes[self.project_buttons[i]]
+                            button.disable()  # disable "Kill" button again
+                            self.project_buttons[
+                                i
+                            ].enable()  # enable main button when "Kill" is pressed
+
+                # check if "New Project" button was pressed
+                if event.ui_element == self.new_project_button:
+                    create_project(self.new_project_name.get_text())
+                    # update folders list
+                    self.update_folders()
 
             self.ui_manager.process_events(event)
 
